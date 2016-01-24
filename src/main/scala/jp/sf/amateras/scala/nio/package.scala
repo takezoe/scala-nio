499c24e23f87b5d2b41499a6697fafc63d3da675
Naoki Takezoe <takezoe@gmail.com>
2016-01-25 01:28:02 +0900

    Add null check to finally block in using

--- src/main/scala/jp/sf/amateras/scala/nio/package.scala
+++ src/main/scala/jp/sf/amateras/scala/nio/package.scala
@@ -8,22 +8,22 @@ package object nio {
   def using[A, Z](r1: Resource[A])(action: (A) => Z): Z = try {
     action(r1.resource)
   } finally {
-    r1.close()
+    if(r1 != null){ r1.close() }
   }
 
   def using[A, B, Z](r1: Resource[A], r2: Resource[B])(action: (A, B) => Z): Z = try {
     action(r1.resource, r2.resource)
   } finally {
-    r1.close()
-    r2.close()
+    if(r1 != null){ r1.close() }
+    if(r2 != null){ r2.close() }
   }
 
   def using[A, B, C, Z](r1: Resource[A], r2: Resource[B], r3: Resource[C])(action: (A, B, C) => Z): Z = try {
     action(r1.resource, r2.resource, r3.resource)
   } finally {
-    r1.close()
-    r2.close()
-    r3.close()
+    if(r1 != null){ r1.close() }
+    if(r2 != null){ r2.close() }
+    if(r3 != null){ r3.close() }
   }
 
   /**
